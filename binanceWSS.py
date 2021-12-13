b24ef8f9e85be3f08ac44a272fab84cea090475d
quadroopl <89741042+quadroopl@users.noreply.github.com>
2021-12-14 03:57:44 +1100

    Update binanceWSS.py

--- binanceWSS.py
+++ binanceWSS.py
@@ -1,6 +1,5 @@
 import time
 import logging
-from time import sleep
 
 from binance import ThreadedWebsocketManager
 
@@ -17,18 +16,11 @@ def run():
 
 
     def handle_socket_message(msg):
-        #print(f"message type: {msg['e']}")
         #print(msg['data'])
-        #logging.info("Binance BUY = "+ str(round(float(msg['data']['b']),4)) + " SELL = " + str(round(float(msg['data']['a']),4)) )
-
-        #print("Binance BUY = "+ str(round(float(msg['data']['b']),4)) + " SELL = " + str(round(float(msg['data']['a']),4)) , end = "\r" )
         global LastBinance
         LastBinance = ("Binance BUY = "+ str(round(float(msg['data']['b']),4)) + " SELL = " + str(round(float(msg['data']['a']),4)) )
-
         #time.sleep(1)
 
-
-    # or a multiplex socket can be started like this
     # see Binance docs for stream names
     streams = ['xrpusdt@bookTicker']
     twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
