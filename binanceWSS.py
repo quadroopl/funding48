ec3c8e1ee4dad62a09274a0ebcaa7b4df4464e45
quadroopl <89741042+quadroopl@users.noreply.github.com>
2021-12-14 03:59:16 +1100

    Update binanceWSS.py

--- binanceWSS.py
+++ binanceWSS.py
@@ -9,17 +9,15 @@ api_secret = None
 LastBinance = "      Waiting for Binance...      "
 
 def run():
-    symbol = 'XRPUSD'
-
+    
     twm = ThreadedWebsocketManager(api_key=api_key, api_secret=api_secret)
     twm.start()
-
 
     def handle_socket_message(msg):
         #print(msg['data'])
         global LastBinance
         LastBinance = ("Binance BUY = "+ str(round(float(msg['data']['b']),4)) + " SELL = " + str(round(float(msg['data']['a']),4)) )
-        #time.sleep(1)
+        #time.sleep(1) ; disabled to keep LastBinance very updated
 
     streams = ['xrpusdt@bookTicker']
     twm.start_multiplex_socket(callback=handle_socket_message, streams=streams)
